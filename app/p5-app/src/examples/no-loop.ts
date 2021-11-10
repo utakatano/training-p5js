864279584cd75afe787386b3b837a3e083d3d57b
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-11-10 23:58:23 +0900

    no-loop

---
+++ app/p5-app/src/examples/no-loop.ts
@@ -0,0 +1,21 @@
+import p5 from "p5"
+
+let y: number
+
+export const setup = (p: p5) => {
+  p.createCanvas(720, 400)
+  p.stroke(255)
+  p.noLoop()
+
+  y = p.height * 0.5
+}
+
+export const draw = (p: p5) => {
+  p.background(0)
+
+  y = y - 1
+  if (y < 0) {
+    y = p.height
+  }
+  p.line(0, y, p.width, y)
+}
