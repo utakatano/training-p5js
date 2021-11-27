74b012cc5a8277739969f71e6b27be175561bb69
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-11-27 23:32:32 +0900

    array

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { draw, setup } from './examples/numbers'
+import { draw, setup } from './examples/array'
 
 const sketch = (p: p5) => {
   p.setup = () => {
