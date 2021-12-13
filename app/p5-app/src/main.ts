19d7d34fc84e17ce84a12d194cec7ce94ccf8f3e
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-12-13 23:49:58 +0900

    create-image

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,8 +1,12 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/image/transparency'
+import { setup, draw } from './examples/image/create-image'
 
 const sketch = (p: p5) => {
+  // p.preload = () => {
+  //   preload(p)
+  // }
+
   p.setup = () => {
     setup(p)
   }
