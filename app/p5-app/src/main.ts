5a5a1744c35b97c8c749aeae45620a3ff94c78a0
kei-yagi <kei.yagi@gmail.com>
2022-01-02 22:22:56 +0900

    sine-cosine

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/math/sine'
+import { setup, draw } from './examples/math/sine-cosine'
 
 const sketch = (p: p5) => {
   // p.preload = () => {
