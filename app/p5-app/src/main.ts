3677cb155de648ae6820010782e2a9ad38b260ec
kei-yagi <kei.yagi@gmail.com>
2022-01-03 23:53:39 +0900

    sine-wave

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/math/sine-cosine'
+import { setup, draw } from './examples/math/sine-wave'
 
 const sketch = (p: p5) => {
   // p.preload = () => {
