a7ba67013e477b380cfcf6a858cca8632a9b1619
kei-yagi <kei.yagi@gmail.com>
2022-01-20 23:36:15 +0900

    objects

--- app/p5-app/src/examples/objects/objects.ts
+++ app/p5-app/src/examples/objects/objects.ts
@@ -14,6 +14,15 @@ class Jitter {
     this.diameter = p.random(10, 30)
     this.speed = 1
   }
+
+  move() {
+    this.x += this.p.random(-this.speed, this.speed)
+    this.y += this.p.random(-this.speed, this.speed)
+  }
+
+  display() {
+    this.p.ellipse(this.x, this.y, this.diameter, this.diameter)
+  }
 }
 
 let bug: Jitter
@@ -21,4 +30,10 @@ let bug: Jitter
 export const setup = (p: p5) => {
   p.createCanvas(710, 400)
   bug = new Jitter(p)
+}
+
+export const draw = (p: p5) => {
+  p.background(50, 89, 100)
+  bug.move()
+  bug.display()
 }
