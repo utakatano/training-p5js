3fdc3f068a9a90d788a2da6098b231b3989188f9
kei-yagi <kei.yagi@gmail.com>
2022-01-25 23:56:57 +0900

    translate

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/objects/objects2'
+import { setup, draw } from './examples/transform/translate'
 
 const sketch = (p: p5) => {
   // p.preload = () => {
