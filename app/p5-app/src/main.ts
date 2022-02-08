c3aec63ecfd25e3c43664a04d1da0a31808304b6
kei-yagi <kei.yagi@gmail.com>
2022-02-08 23:56:03 +0900

    bubble-sort

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/simulate/brownian-motion'
+import { setup, draw } from './examples/simulate/bubble-sort'
 
 const sketch = (p: p5) => {
   // p.preload = () => {
