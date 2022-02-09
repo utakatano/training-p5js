a8f3cc0bd5d609127673bc3f124b3dc5beeb6221
kei-yagi <kei.yagi@gmail.com>
2022-02-09 23:30:11 +0900

    chain

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/simulate/bubble-sort'
+import { setup, draw } from './examples/simulate/chain'
 
 const sketch = (p: p5) => {
   // p.preload = () => {
