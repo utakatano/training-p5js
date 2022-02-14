f44d6ac157a75b32a2b7605f6c590ffe9823d5b7
kei-yagi <kei.yagi@gmail.com>
2022-02-14 23:21:49 +0900

    the-mandelbrot-set

--- app/p5-app/src/main.ts
+++ app/p5-app/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 import p5 from 'p5'
-import { setup, draw } from './examples/simulate/recursive-tree'
+import { setup, draw } from './examples/simulate/the-mandelbrot-set'
 
 const sketch = (p: p5) => {
   // p.preload = () => {
